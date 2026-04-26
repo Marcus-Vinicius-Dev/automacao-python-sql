5f0f2035d3e3bfca2c9353d9bd2922c24d397197
Marcus-Vinicius-Dev <marcus.vini.dev@gmail.com>
2026-04-26 18:37:28 -0300

    PYTHON zddproc2

--- scripts/TESTES.sql
+++ scripts/TESTES.sql
@@ -1,23 +1,7 @@
-DECLARE @GetInstances TABLE (Value nvarchar(100), InstanceNames nvarchar(100), Data nvarchar(100));
-INSERT INTO @GetInstances
-EXECUTE xp_regread
-    @rootkey = 'HKEY_LOCAL_MACHINE',
-    @key = 'SOFTWARE\Microsoft\Microsoft SQL Server',
-    @value_name = 'InstalledInstances'
-SELECT Value, InstanceNames, Data FROM @GetInstances
+USE CONTAGEH
+SELECT *          
+FROM [dbo].[TABELA]
+WHERE NOME LIKE '%NOME114%' 
 
-SELECT  
-    @@SERVERNAME AS NomeServidor,
-    SERVERPROPERTY('MachineName') AS NomeMaquina,
-    SERVERPROPERTY('InstanceName') AS Instancia,
-    SERVERPROPERTY('IsLocalDB') AS IsLocalDB
-
-SELECT              
-   NAME,
-   CREATE_DATE,
-   STATE_DESC
-FROM sys.databases
-ORDER BY NAME
-
-
-SELECT * FROM xp_regread
+   🎯 ENCONTRADO: CEPEA_SAS.dbo.CTG0226.NOME
+   🎯 ENCONTRADO: CONTAGEH.dbo.TABELA.NOME
